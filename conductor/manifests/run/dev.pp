a9e43985597a7fa80db9479a4013d603cb4b8daf
John Eckersberg <jeckersb@redhat.com>
2012-08-23 13:39:39 -0400

    pu

--- conductor/manifests/run/dev.pp
+++ conductor/manifests/run/dev.pp
@@ -2,7 +2,7 @@ class conductor::run::dev {
   require conductor::setup::dev
 
   exec { "conductor rails server":
-    cwd => '/tmp/conductor',
+    cwd => '/tmp/conductor/src',
     command => '/usr/bin/bundle exec rails server'
   }
 }
