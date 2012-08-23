4fcba3f8b6fdb9b226ec96667cc807a007a3bd26
John Eckersberg <jeckersb@redhat.com>
2012-08-23 13:37:07 -0400

    pu

--- conductor/manifests/install/dev.pp
+++ conductor/manifests/install/dev.pp
@@ -6,13 +6,13 @@ class conductor::install::dev {
 
   # converge-ui
   exec { "init submodule":
-    cwd => "/tmp/conductor"
-    command => "/usr/bin/git submodule init"
+    cwd => "/tmp/conductor",
+    command => "/usr/bin/git submodule init",
     require => Git::Repo[conductor]
   }
   exec { "update submodule":
-    cwd => "/tmp/conductor"
-    command => "/usr/bin/git submodule update"
+    cwd => "/tmp/conductor",
+    command => "/usr/bin/git submodule update",
     requier => Exec["init submodule"]
   }  
 }
