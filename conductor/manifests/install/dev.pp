04299adc68f0360640d3073bf1e9ed89d4727d3d
John Eckersberg <jeckersb@redhat.com>
2012-08-23 13:36:43 -0400

    pu

--- conductor/manifests/install/dev.pp
+++ conductor/manifests/install/dev.pp
@@ -3,4 +3,16 @@ class conductor::install::dev {
     src => 'git://github.com/aeolusproject',
     dst => '/tmp'
   }
+
+  # converge-ui
+  exec { "init submodule":
+    cwd => "/tmp/conductor"
+    command => "/usr/bin/git submodule init"
+    require => Git::Repo[conductor]
+  }
+  exec { "update submodule":
+    cwd => "/tmp/conductor"
+    command => "/usr/bin/git submodule update"
+    requier => Exec["init submodule"]
+  }  
 }
