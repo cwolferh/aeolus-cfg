a000072bb27170f996c2de08c7ff405434545ccf
John Eckersberg <jeckersb@redhat.com>
2012-08-23 13:01:13 -0400

    pu

--- conductor/manifests/init.pp
+++ conductor/manifests/init.pp
@@ -1,6 +1,9 @@
 class conductor ($dev = false) {
   if $dev {
     include conductor::install::dev
+    include conductor::config::dev
+    #include conductor::setup::dev
+    #include conductor::run::dev
   } else {
     include conductor::install
   }
